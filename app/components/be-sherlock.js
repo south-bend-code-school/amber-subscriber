c5635d6e3e87ce0a41634481ac7dec58ea1f2971
Bryan Massoth <btmassoth@gmail.com>
2016-10-02 02:06:20 -0400

    finishing

--- app/components/be-sherlock.js
+++ app/components/be-sherlock.js
@@ -11,26 +11,63 @@ export default EmberUploader.FileField.extend({
 
     if(!Ember.isEmpty(files)){
       let photo = files[0];
+      let component = this;
+
+      // loadImage(photo,function(img) {
+      //   let watson = component.get('reportToWatson');
+      //   let storage = component.get('emberStorage');
+      //   console.log(img);
+      //
+      //   let upload = storage.uploadPhoto('/uploads',img.src);
+      //   upload.on('state_changed',null,function(/* error */){
+      //       console.warn("error!");
+      //     },function(/* success */){
+      //       let imageLocation = upload.snapshot.metadata.downloadURLs[0];
+      //
+      //       watson.reportImage(imageLocation);
+      //     });
+      //
+      // },{maxWidth:600});
+      this.set('photo',photo);
       imageResizer.resize(photo, {
         width: 300,
         height: 520
       }, function(blob, didItResize){
+        let watson = component.get('reportToWatson');
+        let storage = component.get('emberStorage');
         if(didItResize){
-          photo = blob;
+          let upload = storage.uploadPhoto('/uploads',blob);
+          upload.on('state_changed',null,function(/* error */){
+              console.warn("error!");
+            },function(/* success */){
+              let imageLocation = upload.snapshot.metadata.downloadURLs[0];
+
+              watson.reportImage(imageLocation);
+            });
+        } else {
+          let upload = storage.uploadPhoto('/uploads',photo);
+          upload.on('state_changed',null,function(/* error */){
+              console.warn("error!");
+            },function(/* success */){
+              let imageLocation = upload.snapshot.metadata.downloadURLs[0];
+
+              watson.reportImage(imageLocation);
+            });
         }
         console.log(blob);
       });
-      let watson = this.get('reportToWatson');
-      let storage = this.get('emberStorage');
-
-      let upload = storage.uploadPhoto('/uploads',photo);
-      upload.on('state_changed',null,function(/* error */){
-          console.warn("error!");
-        },function(/* success */){
-          let imageLocation = upload.snapshot.metadata.downloadURLs[0];
-
-          watson.reportImage(imageLocation);
-        });
+      // let watson = this.get('reportToWatson');
+      // let storage = this.get('emberStorage');
+      //
+      // console.log(photo);
+      // let upload = storage.uploadPhoto('/uploads',this.get('photo'));
+      // upload.on('state_changed',null,function(/* error */){
+      //     console.warn("error!");
+      //   },function(/* success */){
+      //     let imageLocation = upload.snapshot.metadata.downloadURLs[0];
+      //
+      //     watson.reportImage(imageLocation);
+      //   });
     }
 
     router.transitionTo('main.thank-you');
